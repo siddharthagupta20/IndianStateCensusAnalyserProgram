68f026bacd7bbaa0e5903d513161deb4c42a493c
siddharthagupta20 <siddharthagupta20@users.noreply.github.com>
2020-10-19 21:38:28 +0530

    [Siddhartha] created: indian state census analyser program

---
+++ src/main/java/com/cg/iscap/StateCensusAnalyser.java
@@ -0,0 +1,9 @@
+package com.cg.iscap;
+
+public class StateCensusAnalyser {
+	
+	public static void masin(String[] args) {
+		System.out.println("Welcome to Indian State Census Analyser Program.");
+	}
+
+}
