0f291134f86c5ea54890b6e879f7ee4e5b1b7cdf
siddharthagupta20 <siddharthagupta20@users.noreply.github.com>
2020-10-19 23:10:35 +0530

    [Siddhartha] modified: test case for count of states added

--- src/test/java/com/cg/iscap/StateCensusAnalyseTest.java
+++ src/test/java/com/cg/iscap/StateCensusAnalyseTest.java
@@ -1,8 +1,21 @@
 package com.cg.iscap;
 
+import static org.junit.Assert.assertEquals;
+
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+import org.junit.Test;
 
 public class StateCensusAnalyseTest {
 	
+	public static final String INDIA_STATE_CENSUS_DATA = "IndiaStateCensusData.csv";
 	
+	@Test
+	public void givenStatesCensusData_whenRead_shouldReturnCount() {
+		StateCensusAnalyser stateCensusAnalyser=new StateCensusAnalyser();
+		Path p=Paths.get(INDIA_STATE_CENSUS_DATA);
+		assertEquals(29,stateCensusAnalyser.loadCsvFile(p));		
+	}
 	
 }
